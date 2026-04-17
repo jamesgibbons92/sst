49ade96c4412f30329e469f6e5183cc9fdccf813
Victor Navarro <vn4varro@gmail.com>
2026-04-17 18:34:39 +0200

    Fix broken workflow link in function durable docs (#6764)

--- platform/src/components/aws/function.ts
+++ platform/src/components/aws/function.ts
@@ -1473,8 +1473,8 @@ export interface FunctionArgs {
    * Configure the lambda function as a [AWS durable function](https://docs.aws.amazon.com/lambda/latest/dg/durable-functions.html).
    *
    * :::caution
-   * This property is meant to be used internally by [Workflow](/docs/components/aws/workflow/).
-   * Prefer the component if you want to use the [SDK](/docs/components/aws/workflow/#sdk) or if you are not very familiar with durable functions limitations.
+   * This property is meant to be used internally by [Workflow](/docs/component/aws/workflow).
+   * Prefer the component if you want to use the [SDK](/docs/component/aws/workflow#sdk) or if you are not very familiar with durable functions limitations.
    * :::
    */
   durable?:
